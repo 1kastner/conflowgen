88c3323b0d193bcbf1d5f7414fb049f23fe50a0e
Marvin Kastner <marvin.kastner@tuhh.de>
2022-01-04 10:51:29 +0100

    Use float only as typehint

--- conflowgen/domain_models/factories/schedule_factory.py
+++ conflowgen/domain_models/factories/schedule_factory.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import datetime
 import logging
-from typing import List, Tuple, Union
+from typing import List, Tuple, Union, Optional
 
 from conflowgen.domain_models.data_types.mode_of_transport import ModeOfTransport
 from conflowgen.domain_models.large_vehicle_schedule import Destination, Schedule
@@ -18,10 +18,10 @@ class ScheduleFactory:
             service_name: str,
             vehicle_arrives_at: datetime.date,
             vehicle_arrives_at_time: datetime.time,
-            average_vehicle_capacity: int,
-            average_moved_capacity: int,
+            average_vehicle_capacity: float,
+            average_moved_capacity: float,
             next_destinations: Union[List[Tuple[str, float]], None],
-            vehicle_arrives_every_k_days: int | None = None
+            vehicle_arrives_every_k_days: Optional[int] = None
     ) -> None:
         """
 
