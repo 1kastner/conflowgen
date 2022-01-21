33eec886b5a0f1f0c118c370e81d81a203555b38
Marvin Kastner <marvin.kastner@tuhh.de>
2022-01-21 12:33:12 +0100

    Revert "Use newer sphinx version"
    This reverts commit 53074c4c5c8e1ef8bea7f33f4f548c5437a2920b.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
             'pytest-github-actions-annotate-failures',  # turns pytest failures into action annotations
 
             # build documentation
-            'sphinx==4.4',
+            'sphinx',
             'sphinx-rtd-theme',
             'sphinx-toolbox',
             'sphinx-autodoc-typehints',
