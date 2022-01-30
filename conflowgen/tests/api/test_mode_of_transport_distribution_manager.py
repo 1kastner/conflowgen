aebecd8678398106590bc9933de55b36bd6529a6
1kastner <marvin.kastner@tuhh.de>
2022-01-30 17:47:16 +0100

    Create reusable validator (#70)

--- conflowgen/tests/api/test_mode_of_transport_distribution_manager.py
+++ conflowgen/tests/api/test_mode_of_transport_distribution_manager.py
@@ -1,12 +1,11 @@
 import unittest
 
 from conflowgen import ModeOfTransportDistributionManager
-from conflowgen.domain_models.distribution_models.mode_of_transport_distribution import ModeOfTransportDistribution
-from conflowgen.domain_models.distribution_validators.mode_of_transport_distribution_validator import \
-    ModeOfTransportDeliveredWithMissing, ModeOfTransportPickedUpByMissing, \
-    ModeOfTransportProportionOutOfRangeException, ModeOfTransportProportionsUnequalOneException
-from conflowgen.domain_models.distribution_seeders import mode_of_transport_distribution_seeder
 from conflowgen.domain_models.data_types.mode_of_transport import ModeOfTransport
+from conflowgen.domain_models.distribution_models.mode_of_transport_distribution import ModeOfTransportDistribution
+from conflowgen.domain_models.distribution_seeders import mode_of_transport_distribution_seeder
+from conflowgen.domain_models.distribution_validators import DistributionProbabilitiesUnequalOne, \
+    DistributionProbabilityOutOfRange, DistributionElementIsMissingException
 from conflowgen.tests.substitute_peewee_database import setup_sqlite_in_memory_db
 
 
@@ -34,7 +33,7 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
             self.assertAlmostEqual(sum_of_all_proportions, 1)
 
     def test_set_with_missing_keys_first_level(self) -> None:
-        with self.assertRaises(ModeOfTransportDeliveredWithMissing):
+        with self.assertRaises(DistributionElementIsMissingException) as cm:
             self.mode_of_transport_distribution_manager.set_mode_of_transport_distribution(
                 {
                     ModeOfTransport.feeder: {
@@ -48,8 +47,14 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
                 }
             )
 
+        expected_message = (
+            "The distribution {'feeder': '...'} was expected to have the following "
+            "elements: ['truck', 'train', 'feeder', 'deep_sea_vessel', 'barge'] but it "
+            "provided the following elements: ['feeder'].")
+        self.assertEqual(expected_message, str(cm.exception))
+
     def test_set_with_missing_keys_second_level(self) -> None:
-        with self.assertRaises(ModeOfTransportPickedUpByMissing):
+        with self.assertRaises(DistributionElementIsMissingException) as cm:
             self.mode_of_transport_distribution_manager.set_mode_of_transport_distribution(
                 {
                     ModeOfTransport.feeder: {
@@ -74,6 +79,13 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
                     },
                 }
             )
+        expected_message = (
+            "The distribution {'train': '0.20000'} was expected to have the following "
+            "elements: ['truck', 'train', 'feeder', 'deep_sea_vessel', 'barge'] but it "
+            "provided the following elements: ['train']. This is error occurred while "
+            "examining the dependent variable 'feeder'."
+        )
+        self.assertEqual(expected_message, str(cm.exception))
 
     def test_happy_path(self) -> None:
         self.mode_of_transport_distribution_manager.set_mode_of_transport_distribution(
@@ -117,7 +129,7 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
         )
 
     def test_set_with_wrong_proportions(self) -> None:
-        with self.assertRaises(ModeOfTransportProportionOutOfRangeException):
+        with self.assertRaises(DistributionProbabilityOutOfRange) as cm:
             self.mode_of_transport_distribution_manager.set_mode_of_transport_distribution(
                 {
                     ModeOfTransport.feeder: {
@@ -156,9 +168,16 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
                         ModeOfTransport.deep_sea_vessel: 0.2
                     }
                 })
+        expected_exception_message = (
+            'The probability of an element to be drawn must range between 0 and 1 but for '
+            "the element 'train' the probability was 1.1 in the distribution {'train': "
+            "'1.10000', 'truck': '0.20000', 'barge': '0.20000', 'feeder': '0.20000', "
+            "'deep_sea_vessel': '0.20000'}. This is error occurred while examining the "
+            "dependent variable 'feeder'.")
+        self.assertEqual(expected_exception_message, str(cm.exception))
 
-    def test_set_what_do_not_add_up_to_one(self) -> None:
-        with self.assertRaises(ModeOfTransportProportionsUnequalOneException):
+    def test_set_distribution_that_does_not_add_up_to_one(self) -> None:
+        with self.assertRaises(DistributionProbabilitiesUnequalOne) as cm:
             self.mode_of_transport_distribution_manager.set_mode_of_transport_distribution(
                 {
                     ModeOfTransport.feeder: {  # these together are beyond 1
@@ -197,3 +216,10 @@ class TestModeOfTransportDistributionManager(unittest.TestCase):
                         ModeOfTransport.deep_sea_vessel: 0.2
                     }
                 })
+        expected_message = (
+            'The sum of all probabilities should sum to 1 but for the '
+            "distribution {'train': '0.90000', 'truck': '0.90000', 'barge': '0.20000', "
+            "'feeder': '0.20000', 'deep_sea_vessel': '0.20000'} the sum was 2.40000. This "
+            "is error occurred while examining the dependent variable 'feeder'."
+        )
+        self.assertEqual(expected_message, str(cm.exception))
