ecaf137431bbb05b2ef5f566b75a3857565f7df0
ramprasath <ramprasath246@gmail.com>
2022-04-22 15:53:14 +0200

    no_data_graph() type hint

--- conflowgen/reporting/no_data_plot.py
+++ conflowgen/reporting/no_data_plot.py
@@ -1,12 +1,12 @@
 from matplotlib import pyplot as plt
 
 
-def no_data_graph() -> plt.axes:
+def no_data_graph() -> plt.axes.axes:
     axs = plt.subplots()
     no_data_text()
     return axs
 
 
-def no_data_text() -> plt.axes:
+def no_data_text() -> plt.axes.axes:
     ax = plt.text(0.35, 0.5, 'No data available for plotting', dict(size=30))
     return ax
