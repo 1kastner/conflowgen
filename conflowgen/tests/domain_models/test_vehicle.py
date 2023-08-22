2b494bfefd56a1b8064d339640dc3f62bcd1081e
Luc Edes <luc.edes@tuhh.de>
2023-08-22 10:29:15 +0200

    Add vehicle.py tests
    - Tests for repr()
    - Tests for barge

--- conflowgen/tests/domain_models/test_vehicle.py
+++ conflowgen/tests/domain_models/test_vehicle.py
@@ -9,7 +9,7 @@ from conflowgen.domain_models.arrival_information import \
     TruckArrivalInformationForPickup, TruckArrivalInformationForDelivery
 from conflowgen.domain_models.data_types.mode_of_transport import ModeOfTransport
 from conflowgen.domain_models.large_vehicle_schedule import Schedule
-from conflowgen.domain_models.vehicle import Feeder, LargeScheduledVehicle, Truck
+from conflowgen.domain_models.vehicle import Feeder, LargeScheduledVehicle, Truck, Barge
 from conflowgen.tests.substitute_peewee_database import setup_sqlite_in_memory_db
 
 
@@ -44,6 +44,16 @@ class TestTruck(unittest.TestCase):
         )
         self.assertIsNotNone(truck)
 
+    def test_repr(self) -> None:
+        truck = Truck.create(
+            delivers_container=True,
+            picks_up_container=False
+        )
+        self.assertEqual(
+            repr(truck),
+            "<Truck '1'>"
+        )
+
 
 class TestFeeder(unittest.TestCase):
 
@@ -75,4 +85,87 @@ class TestFeeder(unittest.TestCase):
         )
         Feeder.create(
             large_scheduled_vehicle=lsv
+        )
+
+    def test_repr(self) -> None:
+        """Check if feeder can be saved"""
+        now = datetime.datetime.now()
+        schedule = Schedule.create(
+            service_name="MyTestFeederLine",
+            vehicle_type=ModeOfTransport.feeder,
+            vehicle_arrives_at=now,
+            average_vehicle_capacity=1100,
+            average_moved_capacity=200
+        )
+        lsv = LargeScheduledVehicle.create(
+            vehicle_name="TestFeeder1",
+            capacity_in_teu=1000,
+            moved_capacity=200,
+            scheduled_arrival=now,
+            schedule=schedule
+        )
+        feeder = Feeder.create(
+            large_scheduled_vehicle=lsv
+        )
+        self.assertEqual(
+            repr(feeder),
+            "<Feeder: 1>"
+        )
+
+
+class TestBarge(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """Create container database in memory"""
+        sqlite_db = setup_sqlite_in_memory_db()
+        sqlite_db.create_tables([
+            Barge,
+            LargeScheduledVehicle,
+            Schedule,
+        ])
+
+    def test_save_barge_to_database(self) -> None:
+        """Check if barge can be saved"""
+        now = datetime.datetime.now()
+        schedule = Schedule.create(
+            service_name="MyTestBargeLine",
+            vehicle_type=ModeOfTransport.barge,
+            vehicle_arrives_at=now,
+            average_vehicle_capacity=1100,
+            average_moved_capacity=200
+        )
+        lsv = LargeScheduledVehicle.create(
+            vehicle_name="TestBarge1",
+            capacity_in_teu=1000,
+            moved_capacity=200,
+            scheduled_arrival=now,
+            schedule=schedule
+        )
+        Barge.create(
+            large_scheduled_vehicle=lsv
+        )
+
+    def test_repr(self) -> None:
+        """Check if barge can be saved"""
+        now = datetime.datetime.now()
+        schedule = Schedule.create(
+            service_name="MyTestBargeLine",
+            vehicle_type=ModeOfTransport.barge,
+            vehicle_arrives_at=now,
+            average_vehicle_capacity=1100,
+            average_moved_capacity=200
+        )
+        lsv = LargeScheduledVehicle.create(
+            vehicle_name="TestBarge1",
+            capacity_in_teu=1000,
+            moved_capacity=200,
+            scheduled_arrival=now,
+            schedule=schedule
+        )
+        barge = Barge.create(
+            large_scheduled_vehicle=lsv
+        )
+        self.assertEqual(
+            repr(barge),
+            "<Barge: 1>"
         )
