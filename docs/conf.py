0f84b1311cd03a4466ccfeaf6fef8ed7c61e981a
Marvin Kastner <marvin.kastner@tuhh.de>
2023-09-09 12:37:59 +0200

    elaborate on excluded patterns in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,12 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.tools']
+exclude_patterns = [
+    'Thumbs.db', '.DS_Store',  # OS-specific
+    '_build',  # Sphinx-specific
+    '.tools',  # project-specific
+    '.ipynb_checkpoints', '.virtual_documents'  # specific for Jupyter Notebooks
+]
 
 add_module_names = False
 
