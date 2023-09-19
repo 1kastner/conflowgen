9970eefe05ebe345f1f47f656d5c47d356e62184
1kastner <marvin.kastner@tuhh.de>
2023-09-19 22:46:02 +0200

    Use new name for matplotlib style (#198)
    * Use new name for matplotlib style

--- conflowgen/reporting/__init__.py
+++ conflowgen/reporting/__init__.py
@@ -144,7 +144,7 @@ class AbstractReportWithMatplotlib(AbstractReport, metaclass=abc.ABCMeta):
         kwargs.pop("static", None)
         kwargs.pop("display_as_ipython_svg", None)
 
-        with plt.style.context('seaborn-colorblind'):
+        with plt.style.context('seaborn-v0_8-colorblind'):
             self.get_report_as_graph(**kwargs)
             plt.show(block=True)
 
