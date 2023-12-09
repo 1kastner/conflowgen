5e6f193d9fc25e8230934535d2e421b89611bbd4
Marvin Kastner <marvin.kastner@tuhh.de>
2023-12-09 22:17:00 +0100

    Set version number to 2.1.1

--- conflowgen/metadata.py
+++ conflowgen/metadata.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.1"
+__version__ = "2.1.1"
 __license__ = "MIT"
 __description__ = """
 A generator for synthetic container flows at maritime container terminals with a focus on yard operations
