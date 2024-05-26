7d5d74cf3dbc77109f2203f7e214fcf317aada7d
Marvin Kastner <marvin.kastner@tuhh.de>
2024-05-26 21:55:21 +0200

    Fix asserts in brackets

--- conflowgen/application/services/vehicle_capacity_manager.py
+++ conflowgen/application/services/vehicle_capacity_manager.py
@@ -11,7 +11,6 @@ from conflowgen.domain_models.vehicle import LargeScheduledVehicle, AbstractLarg
 
 
 class VehicleCapacityManager:
-
     ignored_capacity = ContainerLength.get_teu_factor(ContainerLength.other)
 
     def __init__(self):
@@ -47,7 +46,7 @@ class VehicleCapacityManager:
         assert vehicle in self.occupied_capacity_for_inbound_journey_buffer, \
             "First .get_free_capacity_for_inbound_journey(vehicle) must be invoked"
 
-        usable_vessel_capacity = self.vehicle_container_volume_calculator.\
+        usable_vessel_capacity = self.vehicle_container_volume_calculator. \
             get_transported_container_volume_on_inbound_journey(vehicle)
 
         occupied_capacity_in_teu = self.occupied_capacity_for_inbound_journey_buffer[vehicle]
@@ -55,14 +54,15 @@ class VehicleCapacityManager:
         new_occupied_capacity_in_teu = occupied_capacity_in_teu + used_capacity_in_teu
 
         new_free_capacity_in_teu = usable_vessel_capacity - new_occupied_capacity_in_teu
-        assert (
-            new_free_capacity_in_teu >= 0,
-            f"vehicle {vehicle} is overloaded, "
-            f"usable_vessel_capacity: {usable_vessel_capacity}, "
-            f"occupied_capacity_in_teu: {occupied_capacity_in_teu}, "
-            f"used_capacity_in_teu: {used_capacity_in_teu}, "
-            f"new_free_capacity_in_teu: {new_free_capacity_in_teu}"
-        )
+        assert \
+            new_free_capacity_in_teu >= 0, \
+            (
+                f"vehicle {vehicle} is overloaded, "
+                f"usable_vessel_capacity: {usable_vessel_capacity}, "
+                f"occupied_capacity_in_teu: {occupied_capacity_in_teu}, "
+                f"used_capacity_in_teu: {used_capacity_in_teu}, "
+                f"new_free_capacity_in_teu: {new_free_capacity_in_teu}"
+            )
 
         self.occupied_capacity_for_inbound_journey_buffer[vehicle] = new_occupied_capacity_in_teu
         vehicle_capacity_is_exhausted = new_free_capacity_in_teu < self.ignored_capacity
@@ -76,7 +76,7 @@ class VehicleCapacityManager:
         assert vehicle in self.occupied_capacity_for_outbound_journey_buffer, \
             "First .get_free_capacity_for_outbound_journey(vehicle) must be invoked"
 
-        scaled_moved_container_volume, unscaled_moved_container_volume = self.vehicle_container_volume_calculator.\
+        scaled_moved_container_volume, unscaled_moved_container_volume = self.vehicle_container_volume_calculator. \
             get_maximum_transported_container_volume_on_outbound_journey(vehicle, container.flow_direction)
 
         # calculate new free capacity
@@ -87,29 +87,29 @@ class VehicleCapacityManager:
         new_scaled_free_capacity_in_teu = scaled_moved_container_volume - new_occupied_capacity_in_teu
 
         new_unscaled_free_capacity_in_teu = unscaled_moved_container_volume - new_occupied_capacity_in_teu
-        assert (
-            new_unscaled_free_capacity_in_teu >= 0,
-            f"vehicle {vehicle} is overloaded, "
-            f"scaled_moved_container_volume: {scaled_moved_container_volume}, "
-            f"unscaled_moved_container_volume: {unscaled_moved_container_volume}, "
-            f"occupied_capacity_in_teu: {occupied_capacity_in_teu}, "
-            f"used_capacity_in_teu: {used_capacity_in_teu}, "
-            f"new_scaled_free_capacity_in_teu: {new_scaled_free_capacity_in_teu} "
-            f"new_unscaled_free_capacity_in_teu: {new_unscaled_free_capacity_in_teu}"
-        )
+        assert \
+            new_unscaled_free_capacity_in_teu >= 0, \
+            (
+                f"vehicle {vehicle} is overloaded, "
+                f"scaled_moved_container_volume: {scaled_moved_container_volume}, "
+                f"unscaled_moved_container_volume: {unscaled_moved_container_volume}, "
+                f"occupied_capacity_in_teu: {occupied_capacity_in_teu}, "
+                f"used_capacity_in_teu: {used_capacity_in_teu}, "
+                f"new_scaled_free_capacity_in_teu: {new_scaled_free_capacity_in_teu} "
+                f"new_unscaled_free_capacity_in_teu: {new_unscaled_free_capacity_in_teu}"
+            )
 
         self.occupied_capacity_for_outbound_journey_buffer[vehicle] = new_occupied_capacity_in_teu
 
         space_is_exhausted = (new_scaled_free_capacity_in_teu <= self.ignored_capacity)
         return space_is_exhausted
 
-    # noinspection PyTypeChecker
     def get_free_capacity_for_inbound_journey(self, vehicle: Type[AbstractLargeScheduledVehicle]) -> float:
         """
         Get the free capacity for the inbound journey on a vehicle that moves according to a schedule in TEU.
         During the ramp-down period (if existent), all inbound traffic is scaled down, no matter what.
         """
-        inbound_container_volume = self.vehicle_container_volume_calculator\
+        inbound_container_volume = self.vehicle_container_volume_calculator \
             .get_transported_container_volume_on_inbound_journey(vehicle)
 
         if vehicle in self.occupied_capacity_for_inbound_journey_buffer:
@@ -125,7 +125,8 @@ class VehicleCapacityManager:
         return free_capacity_in_teu
 
     def get_free_capacity_for_outbound_journey(
-            self, vehicle: Type[AbstractLargeScheduledVehicle],
+            self,
+            vehicle: Type[AbstractLargeScheduledVehicle],
             flow_direction: FlowDirection
     ) -> float:
         """
@@ -148,13 +149,14 @@ class VehicleCapacityManager:
             )
             self.occupied_capacity_for_outbound_journey_buffer[vehicle] = occupied_capacity_in_teu
 
-        assert (
-            unscaled_moved_container_volume - occupied_capacity_in_teu >= 0,
-            f"vehicle {vehicle} is overloaded, "
-            f"maximum_transported_container_volume: {maximum_transported_container_volume}, "
-            f"unscaled_moved_container_volume: {unscaled_moved_container_volume}, "
-            f"occupied_capacity_in_teu: {occupied_capacity_in_teu}"
-        )
+        assert \
+            unscaled_moved_container_volume - occupied_capacity_in_teu >= 0, \
+            (
+                f"vehicle {vehicle} is overloaded, "
+                f"maximum_transported_container_volume: {maximum_transported_container_volume}, "
+                f"unscaled_moved_container_volume: {unscaled_moved_container_volume}, "
+                f"occupied_capacity_in_teu: {occupied_capacity_in_teu}"
+            )
 
         free_capacity = max(maximum_transported_container_volume - occupied_capacity_in_teu, 0)
 
@@ -193,10 +195,11 @@ class VehicleCapacityManager:
         ).count()
         return number_loaded_containers
 
+
     @classmethod
     def _get_number_containers_for_inbound_journey(
             cls,
-            vehicle: AbstractLargeScheduledVehicle,
+            vehicle: Type[AbstractLargeScheduledVehicle],
             container_length: ContainerLength
     ) -> int:
         """Returns the number of containers on a specific vehicle of a specific container length that are delivered by
