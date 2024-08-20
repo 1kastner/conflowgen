257842229b8a1b967212da9c9aa50f5dd59b3a71
Marvin Kastner <marvin.kastner@tuhh.de>
2024-08-20 17:18:25 +0200

    Add Shubhangi as contributor in the docstring

--- conflowgen/metadata.py
+++ conflowgen/metadata.py
@@ -4,6 +4,6 @@ __description__ = """
 A generator for synthetic container flows at maritime container terminals with a focus on yard operations
 """
 
-__author__ = "Marvin Kastner and Ole Grasse"
+__author__ = "Marvin Kastner, Ole Grasse, and Shubhangi Gupta"
 __maintainer__ = "Marvin Kastner"
 __email__ = "marvin.kastner@tuhh.de"
