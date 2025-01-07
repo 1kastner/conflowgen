9b8a99f58947a3b080f5710a7576d1d8a81385a0
Harshitha Prabhuswamy <harshithabilugali@gmail.com>
2025-01-07 18:15:43 +0100

    Added Sphinx configuration to generate MyBinder links in notebooks:
    - Updated `nbsphinx_prolog` in `conf.py` to include GitHub, Binder, and download options for each notebook.
    - Enabled interactive version of notebooks on MyBinder.

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,8 @@ numfig = True
 
 # -- Style nbsphinx notebook rendering ----------------------------------------
 
-nbsphinx_prolog = """
+nbsphinx_prolog = r"""
+{% set docname = 'doc/' + env.doc2path(env.docname, base=None)|string %}
 .. raw:: html
 
     <!-- nbsphinx prolog - start -->
@@ -184,7 +185,39 @@ nbsphinx_prolog = """
 
     </style>
     
-    <!-- nbsphinx prolog - end -->
+    # This is processed by Jinja2 and inserted before each notebook
+
+        <div class="admonition note">
+        This page was generated from
+        <a class="reference external" href="https://github.com/1kastner/conflowgen/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
+        Interactive online version:
+        <span style="white-space: nowrap;"><a href="https://github.com/1kastner/conflowgen/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
+        <a href="{{ env.docname.split('/')|last|e + '.ipynb' }}" class="reference download internal" download>Download notebook</a>.
+        <script>
+            if (document.location.host) {
+            let nbviewer_link = document.createElement('a');
+            nbviewer_link.setAttribute('href',
+                'https://nbviewer.org/url' +
+                (window.location.protocol == 'https:' ? 's/' : '/') +
+                window.location.host +
+                window.location.pathname.slice(0, -4) +
+                'ipynb');
+            nbviewer_link.innerHTML = 'Or view it on <em>nbviewer</em>';
+            nbviewer_link.classList.add('reference');
+            nbviewer_link.classList.add('external');
+            document.currentScript.replaceWith(nbviewer_link, '.');
+            }
+        </script>
+        </div>
+
+    .. raw:: latex
+
+        \nbsphinxstartnotebook{\scriptsize\noindent\strut
+        \textcolor{gray}{The following section was generated from
+        \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+        
+        <!-- nbsphinx prolog - end -->
+
 """
 
 if os.environ.get("IS_RTD", False):
