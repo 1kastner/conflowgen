616b5d4d55004469f0675be11c32e9ce844e52b6
Marvin Kastner <marvin.kastner@tuhh.de>
2025-04-24 21:47:21 +0200

    fix CI

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ with open(os.path.join(this_directory, 'Readme.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Define actual setup
+# Please check pyproject.toml for dependencies etc., most information is kept there
 # noinspection SpellCheckingInspection
 setup(
     name='conflowgen',
@@ -21,7 +22,6 @@ setup(
     long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/1kastner/conflowgen',
-    python_requires='>=3.8',
     license=metadata['__license__'],
     author=metadata['__author__'],
     author_email=metadata['__email__'],
