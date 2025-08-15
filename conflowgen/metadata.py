36980403c0695ae953e4a26fce3e60e79fea50b4
Marvin Kastner <marvin.kastner@tuhh.de>
2025-08-15 11:11:42 +0200

    Fix unittest depending on kaleido (#233)
    * avoid using kaleido during unittests
    
    * Update build pipeline
    
    * Craft version 3.0.1 - now the pip package comes without the static image generation during the unittests

--- conflowgen/metadata.py
+++ conflowgen/metadata.py
@@ -1,4 +1,4 @@
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __license__ = "MIT"
 __description__ = """
 A generator for synthetic container flows at maritime container terminals with a focus on yard operations
